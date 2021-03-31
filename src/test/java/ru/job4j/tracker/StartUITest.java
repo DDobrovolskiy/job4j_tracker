d547b1449365a3ca80a668522dff948a9c8ca3f3
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-03-31 06:56:45 +0500

    5.1. Тестирование. Подготовка данных. [#498098] change

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -43,6 +43,6 @@ public class StartUITest {
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertThat(deleted, nullValue());
+        assertNull(deleted);
     }
 }
