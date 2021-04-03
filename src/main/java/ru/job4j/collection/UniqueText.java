7a8315e83a6279780a63f1121e5f738a3a288238
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-03 19:15:44 +0500

    2. Идентичные тексты [#498082]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = false;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String item :
+                origin) {
+            check.add(item);
+        }
+        rsl = check.containsAll(Arrays.asList(text.clone()));
+        return rsl;
+    }
+}
