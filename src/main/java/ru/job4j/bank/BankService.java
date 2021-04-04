21c30598ddde7cc7e680898b8cfdfc0a064e19ac
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-04 16:42:11 +0500

    3. Банковские переводы [#498078]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -15,64 +15,49 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        try {
             User user = findByPassport(passport);
-            if (!users.get(user).contains(account)){
-                users.get(user).add(account);
+            if (user != null) {
+                if (!users.get(user).contains(account)) {
+                    users.get(user).add(account);
+                }
             }
-        }
-        catch (NullPointerException e) {
-            System.out.println(e.getMessage());
-        }
     }
 
     public User findByPassport(String passport) {
         for (Map.Entry<User, List<Account>> user : users.entrySet()) {
-            if(user.getKey().getPassport().equals(passport)) {
+            if (user.getKey().getPassport().equals(passport)) {
                 return user.getKey();
             }
         }
-        //return null;
-        throw new NullPointerException("User don`t found");
+        return null;
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        try {
             User user = findByPassport(passport);
-            int index = users.get(user).indexOf(new Account(requisite, 0));
-            return users.get(user).get(index);
-        }
-        catch (NullPointerException e) {
-            System.out.println(e.getMessage());
-            //throw new NullPointerException("Account don`t found");
+            if (user != null) {
+                int index = users.get(user).indexOf(new Account(requisite, 0));
+                return users.get(user).get(index);
+            }
             return null;
-        }
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
-        try {
-            Account accountSrc = findByRequisite(srcPassport, srcRequisite);
-            Account accountDest = findByRequisite(destPassport, destRequisite);
-            if (accountSrc.getBalance() < amount) {
-                System.out.println("Pff... your need money");
-                return false;
-            } else {
-                accountSrc.setBalance(accountSrc.getBalance() - amount);
-                accountDest.setBalance(accountDest.getBalance() + amount);
-                return true;
-            }
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
+        Account accountSrc = findByRequisite(srcPassport, srcRequisite);
+        Account accountDest = findByRequisite(destPassport, destRequisite);
+        if (accountSrc == null && accountDest == null) {
             return false;
+        } else if (accountSrc.getBalance() < amount) {
+            System.out.println("Pff... your need money");
+            return false;
+        } else {
+            accountSrc.setBalance(accountSrc.getBalance() - amount);
+            accountDest.setBalance(accountDest.getBalance() + amount);
+            return true;
         }
     }
 
     public static void main(String[] args) {
-        BankService bankService = new BankService();
-        bankService.addAccount("sdfdfs", new Account("g333", 100));
-
-
         List<Account> accounts = new ArrayList<>();
         String requisite = "3fdsbb9";
         accounts.add(new Account("3fdsbb9", 140));
