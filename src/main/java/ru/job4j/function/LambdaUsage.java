ad9fe79c87c6010fa04c4007d4c41f41a3e9d96a
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-08 15:55:57 +0500

    2.2. Лямбда блок [#497893]

---
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -0,0 +1,15 @@
+package ru.job4j.function;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - "
+                    + Integer.parseInt(right)
+                    + " : "
+                    + Integer.parseInt(left));
+            return Integer.compare(Integer.parseInt(right),Integer.parseInt(left));
+        };
+    }
+}
