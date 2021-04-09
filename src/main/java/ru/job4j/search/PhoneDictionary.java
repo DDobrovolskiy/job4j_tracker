ffe7b43c5c06b63691c40b89a9f40e98acfbe426
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-09 11:09:36 +0500

    6. Функции высшего порядка [#497887]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,8 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -14,16 +16,31 @@ public class PhoneDictionary {
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
+//   public ArrayList<Person> find(String key) {
+//       ArrayList<Person> result = new ArrayList<>();
+//       for (Person person : persons) {
+//           if (person.getName().contains(key)
+//                   || person.getSurname().contains(key)
+//                   || person.getPhone().contains(key)
+//                   || person.getAddress().contains(key)) {
+//               result.add(person);
+//           }
+//           }
+//       return result;
+//   }
+
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine =
+                ((Predicate<Person>) person -> person.getName().contains(key))
+                        .or(person -> person.getPhone().contains(key))
+                        .or(person -> person.getSurname().contains(key))
+                        .or(person -> person.getAddress().contains(key));
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getPhone().contains(key)
-                    || person.getAddress().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
-            }
+        }
         return result;
     }
 }
