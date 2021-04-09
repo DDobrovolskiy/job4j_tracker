1495065c92d923b949644a14fa7abe9b91b75fcd
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-09 18:18:39 +0500

    5. Подсчет функции в диапазоне. [#497886]

--- src/test/java/ru/job4j/lambda/LambdaTest.java
+++ src/test/java/ru/job4j/lambda/LambdaTest.java
@@ -11,31 +11,24 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class LambdaTest {
-    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
-        List<Double> rsl = new ArrayList<>();
-        for (int i = start; i < end; i++) {
-            rsl.add(func.apply((double) i));
-        }
-        return rsl;
-    }
 
     @Test
     public void whenLinearFunctionThenLinearResults() {
-        List<Double> result = diapason(5, 8, x -> 2 * x + 1);
+        List<Double> result = Lambda.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenLinearFunctionThenKvadratResults() {
-        List<Double> result = diapason(0, 3, x -> x * x);
+        List<Double> result = Lambda.diapason(0, 3, x -> x * x);
         List<Double> expected = Arrays.asList(0D, 1D, 4D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenLinearFunctionThenPokazatelResults() {
-        List<Double> result = diapason(0, 3, x -> Math.pow(2, x));
+        List<Double> result = Lambda.diapason(0, 3, x -> Math.pow(2, x));
         List<Double> expected = Arrays.asList(1D, 2D, 4D);
         assertThat(result, is(expected));
     }
