b50960db5a95a26fb58259476f405d474e3b28e1
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-11 23:25:37 +0500

    8. Перенос методов в Stream API [#497908]

---
+++ src/main/java/ru/job4j/lambda/Card.java
@@ -0,0 +1,31 @@
+package ru.job4j.lambda;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        List<Card> cards = Stream.of(Suit.values())
+                .flatMap(suit1 -> Stream.of(Value.values())
+                .map(value1 -> new Card(suit1, value1)))
+                .collect(Collectors.toList());
+        System.out.println(cards);
+    }
+}
