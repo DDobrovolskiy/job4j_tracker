768a9c8103140985d7675ce2b54b15766a590c9a
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-12 19:27:05 +0500

    1. Экзамен. Коллекции. [#498060]

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -7,21 +7,17 @@ import java.util.Map;
 public class FreezeStr {
     public static boolean eq(String left, String right) {
         Map<String, Integer> mapLeft = new HashMap<>();
-        Map<String, Integer> mapRight = new HashMap<>();
         Arrays.asList(left.split("")).stream()
                 .forEach(item -> {
                     mapLeft.computeIfPresent(item, (key, value) -> value + 1);
                     mapLeft.putIfAbsent(item, 1);
                 });
-        Arrays.asList(right.split("")).stream()
-                .forEach(item -> {
-                    mapRight.computeIfPresent(item, (key, value) -> value + 1);
-                    mapRight.putIfAbsent(item, 1);
-                });
-        for (String key : mapRight.keySet()) {
+        for (String key : Arrays.asList(right.split(""))) {
             if (mapLeft.containsKey(key)) {
-                if (mapLeft.get(key) < mapRight.get(key)) {
-                    return false;
+                if (mapLeft.get(key) - 1 == 0) {
+                    mapLeft.remove(key);
+                } else {
+                    mapLeft.put(key, mapLeft.get(key) - 1);
                 }
             } else {
                 return false;
